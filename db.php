3ee0396d901e0c1660964d38693a0e0d277164b1
Liliana Baltazar <a28892@alunos.ipb.pt>
2020-01-14 02:26:16 +0000

    adicionado tema para o backoffice criada coneccao com a base de dados sql da base de dados com um utilizador de 'admin'

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Author: Liliana Baltazar
+*/
+
+// Enter your Host, username, password, database below.
+// I left password empty because i do not set password on localhost.
+$con = mysqli_connect("localhost","root","","projeto_multimedia");
+	if (mysqli_connect_errno()){
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		die();
+		}
+?>
