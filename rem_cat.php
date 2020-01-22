aa54ce10a99610e9077431acb8f7bc3c6db2fdf1
nmaximo <58787383+nmaximo@users.noreply.github.com>
2020-01-22 19:05:47 +0000

    Add files via upload

---
+++ rem_cat.php
@@ -0,0 +1,11 @@
+<?php
+// liga��o base dados
+require('ligacao_bd.php');
+
+// consulta para apagar evento registado
+$sql = "DELETE FROM categoria WHERE categoria = " . $_GET['categoria'];
+$consulta = mysqli_query($sql);
+header("Location: menu_admin.php");
+exit;
+
+?>
