f85b054c318d978a6f3bfcb1ed7b423949f68219
Liliana Baltazar <lilianaluisabaltazar@gmail.com>
2020-02-11 10:39:51 +0000

    Removido codigo duplicado do footer

--- computador.php
+++ computador.php
@@ -322,33 +322,9 @@ mysqli_close($link);
     </div>
 </div>
 
-<!-- Footer -->
-
-<div class="footer_overlay"></div>
-<footer class="footer">
-    <div class="footer_background" ></div>
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="footer_content d-flex flex-lg-row flex-column align-items-center justify-content-lg-start justify-content-center" style="background-color: white">
-                    <div class="footer_logo"><a href="#"><img height="40%" src="images/exe1.png" width="60%" alt=""></a></div>
-                    <div class="copyright ml-auto mr-auto"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-                    <div class="footer_social ml-lg-auto">
-                        <ul>
-                            <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-</div>
+<?php
+include('footer.php');
+?>
 
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="styles/bootstrap4/popper.js"></script>
